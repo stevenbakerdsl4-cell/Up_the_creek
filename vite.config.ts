b3fe4a0037da79af7261292c9b2164348777c536
stevenbakerdsl4-cell <stevenbakerdsl4@gmail.com>
2026-08-17 23:53:08 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // <-- ADD THIS LINE
   plugins: [react()],
   resolve: {
     alias: {
